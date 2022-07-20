1df97a8473a5c5c108071f793d28000a6864e95e
joetrops5 <jtropss@aol.com>
2022-07-19 23:45:41 -0400

    added to javascript, html and css files

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # Joes-Code-Quiz
 
-# How your app works
-1. on page open initialize quize
-2. states: loading, engaged, ended
-3. loading: fetches questions, sets timer, sets score
 
--- script.js
+++ script.js
@@ -1,28 +1,127 @@
-// var hide= document.querySelector(".start")
 const startBtn = document.querySelector(".start");
 const questionDiv = document.querySelector(".question");
 const timerDiv = document.querySelector(".timer");
+const answerText = document.querySelector(".answer");
+const highScoresSection = document.querySelector(".high_scores");
+
+// {"initials": score}
+let highScores = {};
+
+function renderHighScores() {
+  highScoresSection.innerHTML = "";
+
+  const h2 = document.createElement("h2");
+  h2.innerHTML = "High Scores";
+  highScoresSection.append(h2);
+
+  for (let initials in highScores) {
+    highScoresSection.append(initials, " - ", highScores[initials]);
+    highScoresSection.innerHTML += "</br>";
+  }
+}
+
+let interval = null;
+
+function stopTimer() {
+  clearInterval(interval);
+  interval = null;
+}
 
 let timeLeft = 0;
 
 function renderTimer() {
   // Clear the old stuff
-  questionDiv.innerHTML = "";
+  timerDiv.innerHTML = "";
   timerDiv.append(`Timer: ${timeLeft}s`);
 }
 
-let currentQuestion = null;
+let currentQuestionIndex = null;
 
 function renderQuestion() {
+  if (currentQuestionIndex >= questions.length) {
+    return;
+  }
+
+  const question = questions[currentQuestionIndex];
+
   // Clear the old stuff
   questionDiv.innerHTML = "";
-  questionDiv.append(currentQuestion.text);
-  // TODO: Create radio buttons
+  questionDiv.append(question.text);
+
+  const form = document.createElement("form");
+
+  for (const key in question.options) {
+    const radioButton = document.createElement("input");
+    radioButton.type = "radio";
+    radioButton.name = "answer";
+    radioButton.value = key;
+    // Require the user to choose an option before submitting
+    radioButton.required = true;
+
+    const label = document.createElement("label");
+    label.append(radioButton);
+
+    label.append(" " + question.options[key]);
+
+    form.append(label);
+  }
+
+  const button = document.createElement("button");
+  button.innerHTML = "Submit";
+  form.append(button);
+
+  form.addEventListener("submit", (e) => {
+    // Prevent the page from reloading
+    e.preventDefault();
+
+    const selectedButton = document.querySelector("[name='answer']:checked");
+    if (selectedButton.value != question.answer) {
+      timeLeft -= 10;
+      renderTimer();
+    }
+
+    if (currentQuestionIndex == questions.length - 1) {
+      stopTimer();
+      renderGameOver();
+    }
+
+    currentQuestionIndex++;
+    renderQuestion();
+  });
+
+  questionDiv.append(form);
+}
+
+function renderGameOver() {
+  // Clear the old stuff
+  questionDiv.innerHTML = "";
+
+  const form = document.createElement("form");
+  const textbox = document.createElement("input");
+  textbox.type = "text";
+  textbox.placeholder = "Initials...";
+  textbox.required = true;
+  form.append(textbox);
+
+  const button = document.createElement("button");
+  button.innerHTML = "Submit";
+  form.append(button);
+
+  form.addEventListener("submit", (e) => {
+    // Prevent the page from reloading
+    e.preventDefault();
+
+    highScores[textbox.value] = timeLeft;
+    renderHighScores();
+    questionDiv.innerHTML = "Thanks for playing!";
+  });
+
+  questionDiv.append(form);
 }
 
 startBtn.addEventListener("click", () => {
   // Prevent starting another interval
-  if (timeLeft > 0) {
+  if (interval != null) {
     return;
   }
 
@@ -30,44 +129,25 @@ startBtn.addEventListener("click", () => {
 
   renderTimer();
 
-  const interval = setInterval(() => {
+  interval = setInterval(() => {
     if (timeLeft <= 0) {
       // Stop the interal from repeating
-      clearInterval(interval);
+      stopTimer();
       return;
     }
-                            
 
     // 1. Set the state
     timeLeft--;
+
+    if (timeLeft <= 0) {
+      renderGameOver();
+    }
 
     // 2. Re-render
     renderTimer();
   }, 1000);
 
-  currentQuestion = questions[0];
-
+  currentQuestionIndex = 0;
   renderQuestion();
 
-  // currentQuestion = questions[1];
-
-  // renderQuestion();
-
-  // currentQuestion = questions[2];
-
-  // renderQuestion();
-
-  // currentQuestion = questions[3];
-
-  // renderQuestion();
-
-  // currentQuestion = questions[4];
-
-  // renderQuestion();
-
-  // if (questionDiv.style.display === "none") {
-  //   questionDiv.style.display = "block";
-  // } else {
-  //   questionDiv.style.display = "none";
-  // }
 });
